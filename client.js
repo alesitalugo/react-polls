02c70bb0edfc69b28fee0b69802becbd7c220f32
alesitalugo <alesitalugo@gmail.com>
2016-04-05 23:19:02 -0500

    -Setting up react and webpack

---
+++ client.js
@@ -0,0 +1,5 @@
+'use strict'
+const React = require('react')
+const App = require('./components/App')
+
+React.render(<App />, document.getElementById('app'))
---
+++ components/App.js
@@ -0,0 +1,9 @@
+'use strict'
+const React = require('react')
+const App = React.createClass({
+  render () {
+    return (<h1>hello world from react </h1>)
+  }
+})
+
+module.export = App
